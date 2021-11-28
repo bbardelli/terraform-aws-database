6bcbd9ce1f7fcac117a3c77ab4ec72a6644e306d
Bruno Augusto Bardelli <brunobardelli@gmail.com>
2021-11-28 19:15:04 +0100

    fix

--- db.tf
+++ db.tf
@@ -2,17 +2,24 @@ resource "aws_dynamodb_table" "application-dynamodb-table" {
   name           = "application_table"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "appName"
-
+  range_key      = "comment"
 
   attribute {
     name = "appName"
     type = "S"
   }
+
+  attribute {
+    name = "comment"
+    type = "S"
+  }
+
 }
 
 resource "aws_dynamodb_table_item" "test" {
   table_name = aws_dynamodb_table.application-dynamodb-table.name
   hash_key   = aws_dynamodb_table.application-dynamodb-table.hash_key
+  range_key = aws_dynamodb_table.application-dynamodb-table.range_key
 
   item = <<ITEM
 {
